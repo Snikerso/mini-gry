085a6dd7736507b3f09ad7461b376e1ba341e3c4
HuminPiotr <humin.piotr@gmail.com>
2022-12-16 19:28:55 +0100

    h1

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+    <h1>ELO ELO 420</h1>
       <p>mmmmmm</p>
       <img src="https://i.pinimg.com/564x/de/2e/79/de2e7967bca627096aed5ff4764a9250.jpg" />
+
+
     </div>
   );
 }
