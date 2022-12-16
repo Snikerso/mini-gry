38c5bb4c233127c592b1cc506cef568de1bb02d9
bartekbf10 <bartekbf10@gmail.com>
2022-12-16 19:49:40 +0100

    cos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h1>toster w wannie</h1>
+      <h1>ELO ELO 420</h1>
       <p>mmmmmm</p>
-      <p>nie róbcie w domu</p>
-      <p>sprawdzam</p>
-
-
+      <img src="https://i.pinimg.com/564x/de/2e/79/de2e7967bca627096aed5ff4764a9250.jpg" />
+      <img src="https://gfx.antyradio.pl/var/antyradio2/storage/images/filmy-i-seriale/filmy/gwiazda-gremlinow-uwaza-ze-gizmo-jest-slodszy-od-baby-yody-wyglada-jakby-go-ogolili-46343/15252485-1-pol-PL/Gwiazda-Gremlinow-uwaza-ze-Gizmo-jest-slodszy-od-Baby-Yody-Wyglada-jakby-go-ogolili_size-360x270.jpg" />
     </div>
   );
 }
