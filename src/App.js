09fe23a2b6ad52700eb7e333900f88f4ddf7ef22
TimeRul3r <120742919+TimeRul3r@users.noreply.github.com>
2022-12-16 19:47:00 +0100

    zmiana

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>ELO ELO 420</h1>
+    <h1>toster w wannie</h1>
       <p>mmmmmm</p>
-      <p>mój komentarz</p>
-      <img src="https://i.pinimg.com/564x/de/2e/79/de2e7967bca627096aed5ff4764a9250.jpg" />
+      <p>nie róbcie w domu</p>
+      <p>sprawdzam</p>
 
 
     </div>
